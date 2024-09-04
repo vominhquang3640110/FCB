7fc858b2629b8dd44d4de2e1f771607c9073d597
vominhquang3640110 <vominhquang3640110@gmail.com>
2024-09-04 16:26:30 +0900

    test

--- js/top.js
+++ js/top.js
@@ -1,19 +1,19 @@
 var windownButton = document.getElementById('windown_button');
 
 windownButton.onmouseover = function() {
-	this.src = '../picture/windows-download-button(2).png';
+	this.src = 'picture/windows-download-button(2).png';
 };
 
 windownButton.onmouseout = function() {
-	this.src = '../picture/windows-download-button(1).png';
+	this.src = 'picture/windows-download-button(1).png';
 };
 
 var androidButton = document.getElementById('android_button');
 
 androidButton.onmouseover = function() {
-	this.src = '../picture/android-download-button(2).png';
+	this.src = 'picture/android-download-button(2).png';
 };
 
 androidButton.onmouseout = function() {
-	this.src = '../picture/android-download-button(1).png';
+	this.src = 'picture/android-download-button(1).png';
 };
